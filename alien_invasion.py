e7f983df5b695e6e08a36546ac81add136e9eb4e
kingpoem <1322153392@qq.com>
2024-03-04 17:03:31 +0800

    创建settings类

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,23 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        #设置背景色
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -19,8 +27,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color)
+
             #让最近绘制的屏幕可见
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #船舰游戏实例并运行游戏
